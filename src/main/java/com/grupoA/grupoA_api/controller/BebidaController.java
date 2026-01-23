6c479d1bc6a34bb9231037f3b1bb76fc1b25f36f
endiva <diazvalenzuelaenrique@gmail.com>
2026-01-23 17:08:21 +0100

    BebidaController

--- src/main/java/com/grupoA/grupoA_api/controller/BebidaController.java
+++ src/main/java/com/grupoA/grupoA_api/controller/BebidaController.java
@@ -1,9 +1,7 @@
 package com.grupoA.grupoA_api.controller;
 
 import com.grupoA.grupoA_api.entity.Bebida;
-import com.grupoA.grupoA_api.repository.BebidaRepository;
 import com.grupoA.grupoA_api.service.BebidaService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,25 +10,41 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/bebidas")
 public class BebidaController {
-
+    //Atributos
     private final BebidaService bebidaService;
 
+    //Constructor
     public BebidaController(BebidaService bebidaService) {
         this.bebidaService = bebidaService;
     }
 
+    //region GET
+    /**
+     * @return Devuelve todas las bebidas
+     */
     @GetMapping
-    public List<Bebida> bebidas() {
+    public List<Bebida> getBebidas() {
         return bebidaService.listarTodos();
     }
 
+    /**
+     * @param id
+     * @return Devuelve una única bebida o nada
+     */
     @GetMapping("/{id}")
-    public Optional<Bebida> obtenerBebidas(@PathVariable Long id) {
+    public Optional<Bebida> getBebidaPorId(@PathVariable Long id) {
         return bebidaService.buscarPorId(id);
     }
+    //endregion
 
+    //region POST
+    /**
+     * @param bebida
+     * @return Inserta una bebida en la BBDD y devuelve dicha bebida
+     */
     @PostMapping
-    public Bebida crear(@RequestBody Bebida bebida) {
+    public Bebida setBebida(@RequestBody Bebida bebida) {
         return bebidaService.guardar(bebida);
     }
+    //endregion
 }
