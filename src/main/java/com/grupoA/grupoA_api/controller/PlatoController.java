2e7f10fc1611b383cd91261d3384b86b0348ea29
endiva <diazvalenzuelaenrique@gmail.com>
2026-01-23 17:18:52 +0100

    PlatoController

--- src/main/java/com/grupoA/grupoA_api/controller/PlatoController.java
+++ src/main/java/com/grupoA/grupoA_api/controller/PlatoController.java
@@ -1,5 +1,50 @@
 package com.grupoA.grupoA_api.controller;
 
-public class PlatoController {
+import com.grupoA.grupoA_api.entity.Plato;
+import com.grupoA.grupoA_api.service.PlatoService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/platos")
+public class PlatoController {
+    //Atributos
+    private final PlatoService platoService;
+
+    //Constructor
+    public PlatoController(PlatoService platoService) {
+        this.platoService = platoService;
+    }
+
+    //region GET
+    /**
+     * @return Devuelve todos los platos
+     */
+    @GetMapping
+    public List<Plato> getPlatos() {
+        return platoService.listarTodos();
+    }
+
+    /**
+     * @param id
+     * @return Devuelve un único plato o nada
+     */
+    @GetMapping("/{id}")
+    public Optional<Plato> getPlatoPorId(@PathVariable Long id) {
+        return platoService.buscarPorId(id);
+    }
+    //endregion
+
+    //region POST
+    /**
+     * @param plato
+     * @return Inserta un plato en la BBDD y devuelve dicho plato
+     */
+    @PostMapping
+    public Plato setPlato(@RequestBody Plato plato) {
+        return platoService.guardar(plato);
+    }
+    //endregion
 }
