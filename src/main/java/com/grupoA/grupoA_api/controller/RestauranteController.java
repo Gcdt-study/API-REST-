cb6bb3e4f22dcb67ca34164c21fd4d25ba9da915
endiva <diazvalenzuelaenrique@gmail.com>
2026-01-23 18:05:17 +0100

    RestauranteController

--- src/main/java/com/grupoA/grupoA_api/controller/RestauranteController.java
+++ src/main/java/com/grupoA/grupoA_api/controller/RestauranteController.java
@@ -1,11 +1,14 @@
 package com.grupoA.grupoA_api.controller;
 
+import com.grupoA.grupoA_api.entity.Empleado;
 import com.grupoA.grupoA_api.entity.Restaurante;
 import com.grupoA.grupoA_api.service.RestauranteService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/api/restaurante")
 public class RestauranteController {
     //Atributos
     private final RestauranteService restauranteService;
@@ -14,11 +17,21 @@ public class RestauranteController {
     public RestauranteController(RestauranteService restauranteService) {
         this.restauranteService = restauranteService;
     }
-    /*
+
     //region GET
+    /**
+     * @return Devuelve los datos de nuestro restaurante
+     */
     @GetMapping
-    public Optional<Restaurante> getRestaurante() {
-        return restauranteService.
-    }*/
+    public Restaurante getRestaurante() {
+        return restauranteService.obtenerDatos();
+    }
+    //endregion
+
+    //region PUT
+    @PutMapping
+    public Restaurante actualizarRestaurante(@RequestBody Restaurante restaurante) {
+        return restauranteService.actualizarDatos(restaurante);
+    }
     //endregion
 }
