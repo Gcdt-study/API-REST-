a0ed3e9398926ce3bff489b31bc6d95977475db7
endiva <diazvalenzuelaenrique@gmail.com>
2026-01-23 17:26:14 +0100

    RestauranteController sin acabar

--- src/main/java/com/grupoA/grupoA_api/controller/RestauranteController.java
+++ src/main/java/com/grupoA/grupoA_api/controller/RestauranteController.java
@@ -1,5 +1,24 @@
 package com.grupoA.grupoA_api.controller;
 
-public class RestauranteController {
+import com.grupoA.grupoA_api.entity.Restaurante;
+import com.grupoA.grupoA_api.service.RestauranteService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
+public class RestauranteController {
+    //Atributos
+    private final RestauranteService restauranteService;
+
+    //Constructor
+    public RestauranteController(RestauranteService restauranteService) {
+        this.restauranteService = restauranteService;
+    }
+    /*
+    //region GET
+    @GetMapping
+    public Optional<Restaurante> getRestaurante() {
+        return restauranteService.
+    }*/
+    //endregion
 }
